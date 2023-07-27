9cbe41aa124eb3b5664ce96b8e936f1f2aa617a6
seo-K <dkasid@naver.com>
2023-07-28 00:07:43 +0900

    feat: mypage api 연결작업중

--- src/routes/my/MyPage.jsx
+++ src/routes/my/MyPage.jsx
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 export default function MyPage() {
   const navigate = useNavigate();
   // const goWhere = () => {
   //   return navigate("/company/registerPost");
   // };
+
+  const [myInfo, setMyInfo] = useState([]); // myInfo 담는 곳
+
+  useEffect(() => {
+    const url = `https://www.tistory.com/apis/blog/info?access_token=${process.env.REACT_APP_TISTORY_TOKEN}&output=json`;
+    axios
+      .get(url)
+      .then(function (response) {
+        const { item } = response.data.tistory;
+        console.log(item);
+      })
+      .catch(function (error) {
+        console.log("실패");
+      });
+  }, []);
 
   return (
     <Container>
